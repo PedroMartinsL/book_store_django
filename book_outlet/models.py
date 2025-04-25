7d894b546c6d7fd803ea69a99c6d5b3b27b9904f
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-04-25 16:46:18 -0300

    Model URLs and redirect

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
 
 # Create your models here.
 
@@ -11,6 +12,9 @@ class Book(models.Model):
     ])
     author = models.CharField(null=True, max_length=50, default='Unknown')
     is_bestselling = models.BooleanField(default=False)
+
+    def get_deferred_fields(self):
+        return reverse('book_detail', args=[self.id])
 
     def __str__(self):
         return self.title
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from.models import Book
@@ -7,3 +8,12 @@ def index(request):
     books = Book.objects.all()
     return render(request, 'book_outlet/index.html',
                   {'books': books})
+
+def book_detail(request, book_id):
+    # try:
+    #     book = Book.objects.get(id=book_id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'book_outlet/book_detail.html',
+                  {'book': book})#can use pk too
